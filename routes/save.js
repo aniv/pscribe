044c15abdc6a38f5ee103fbfe9e524cdafe1f2af
Ani Vemprala <ani@aniv.info>
2012-12-24 23:24:45 -0500

    appfog mongo persistence setup - vcap

--- routes/save.js
+++ routes/save.js
@@ -5,8 +5,7 @@ exports.home = function(req, res){
 exports.save = function(req, res){
 	var mongo = require('mongodb').MongoClient;
 	res.set('Content-Type', 'application/json');
-	
-	mongo_conn = process.env.VCAP_SERVICES['mongodb-1.8'].credentials.url; //'mongodb://localhost:27017/prsm'
+	mongo_conn = process.env.VCAP_SERVICES['mongodb-1.8'][0]['credentials']['url']; //'mongodb://localhost:27017/prsm'
 
 	mongo.connect(mongo_conn, function(err, db) {
 		if (err) {
