693eb352c2fb471c1d001571aafddeb41d2d94e5
Ani Vemprala <ani@aniv.info>
2012-12-24 22:40:54 -0500

    barebones service; writes json to mongodb against /save route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , persist = require('./routes/save')
   , http = require('http')
   , path = require('path');
 
@@ -28,7 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/save', persist.save);
+app.get('/save', persist.home);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ routes/save.js
@@ -0,0 +1,47 @@
+exports.home = function(req, res){
+	res.render('index', { title: 'Save' });
+};
+
+exports.save = function(req, res){
+	var mongo = require('mongodb').MongoClient;
+	res.set('Content-Type', 'application/json');
+
+	mongo.connect('mongodb://localhost:27017/prsm', function(err, db) {
+		if (err) {
+			console.log("Unable to connect to db; error: ");
+			console.dir(err);
+		}
+		
+		var prsm = req.body;
+		var collection;
+		
+		switch ('dev') //prsm.interest.key
+		{
+			case 'share':
+				collection = db.collection('shared');
+				break;
+			case 'recommend':
+				collection = db.collection('recommended');
+				break;
+			case 'save':
+				collection = db.collection('saved');
+				break;
+			case 'dev':
+				collection = db.collection('prsm');
+				break;
+			default:
+				collection = db.collection('default');
+		}
+		
+		collection.insert(prsm.docs, {safe:false}, function(err, result) { 
+			if (err) { 
+				console.log("Failed to save records"); 
+				res.send("{'Status': 'ERROR'}");
+			}
+			else {
+				console.log("Saved all records");
+				res.send("{'Status': 'OK'}");
+			}
+		});
+	});
+}
